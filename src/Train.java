04afb36d4702993eb1d703d90985fa04da688b8f
RohanAM6606 <rohan02aug@gmail.com>
2026-03-30 14:13:42 +0530

    Initialised application with welcome message

---
+++ src/Train.java
@@ -0,0 +1,8 @@
+public class Train {
+    public static void main(String[] args){
+        System.out.println("Welcome to TrainConsistManagement");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully.");
+
+    }
+}
